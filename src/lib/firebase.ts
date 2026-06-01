afe59b4996db4a864e158cdfb83205b2c220af40
hnethercutt <hconran@gmail.com>
2026-06-01 19:36:00 -0400

    Added firebase config

---
+++ src/lib/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Configures firebase with local (public) environmental variables
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+/*
+ * Start and authenticate firebase with the provided configurations
+ * Check if app already initialized to prevent happening multiple times from components importing this file
+*/
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+// Export for use elsewhere in the client side of the app
+export { app, auth, provider };
