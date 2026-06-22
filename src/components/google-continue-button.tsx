02d25d44421f2850874a866bb85439837ae9a31a
hnethercutt <hconran@gmail.com>
2026-06-21 22:06:57 -0400

    Moved continue with google logic to service

--- src/components/google-continue-button.tsx
+++ src/components/google-continue-button.tsx
@@ -1,9 +1,6 @@
 'use client'
-
-import { signInWithPopup } from 'firebase/auth';
-import { auth, provider } from '../lib/firebase';
 import { useRouter } from 'next/navigation';
-import { createUser } from '@/services/user-service';
+import { signInWithGoogle } from '@/services/user-service';
 import Image from 'next/image';
 import './google-continue-button.css';
 
@@ -11,18 +8,8 @@ export default function GoogleContinueButton() {
     const router = useRouter();
 
     const continueWithGoogle = async () => {
-        /*
-         * Displays googles login page in a pop up and handles the result
-         * It runs the same for a sign up, so we only need one function for this
-        */
-        signInWithPopup(auth, provider).then((_result) => {
-            // Add user to the firestore database
-            return createUser(_result.user);
-        }).then(() => {
-            // Return to home page if/when authentication is successful
+        signInWithGoogle().then(() => {
             router.push('/');
-        }).catch((err) => {
-            console.error(err.message);
         });
     };
 
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,7 +1,8 @@
 import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
-import { db } from '@/lib/firebase';
-import { User } from 'firebase/auth';
+import { auth, db, provider } from '@/lib/firebase';
+import { signInWithPopup, User } from 'firebase/auth';
 
+// Adds the user to firestore database
 export async function createUser(user: User) {
     // A new user doc is only created if one doesn't already exists with the given uid
     await setDoc(doc(db, 'users', user.uid), {
@@ -10,4 +11,16 @@ export async function createUser(user: User) {
         displayName: user.displayName,
         email: user.email
     }, { merge: true });
+}
+
+/*
+* Displays googles login page in a pop up and handles the result
+* It runs the same for a sign up, so we only need one function for this
+*/
+export async function signInWithGoogle() {
+    signInWithPopup(auth, provider).then((_result) => {
+        return createUser(_result.user);
+    }).catch((err) => {
+        console.error(err.message);
+    });
 }
