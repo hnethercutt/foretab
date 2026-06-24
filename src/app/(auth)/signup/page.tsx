0dcfae0eb86531b1268672df4951485a806bb832
hnethercutt <hconran@gmail.com>
2026-06-24 16:37:46 -0400

    Removed imports I no longer need

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,6 @@
 'use client';
-import styles from '../auth.module.css';
-import Form from 'next/form';
 import GoogleContinueButton from '@/components/google-continue-button';
 import SignupForm from '@/components/signup-form';
-import { useState } from 'react';
-import { Eye, EyeOff } from 'lucide-react';
-import { Tooltip } from '@mui/material';
 
 export default function Signup() {
   return (
