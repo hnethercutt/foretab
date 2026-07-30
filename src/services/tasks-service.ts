6ad07615a7118371dfa5534bd8836ac4514b6fc9
hnethercutt <hconran@gmail.com>
2026-07-30 13:12:40 -0400

    Added logic to get and update task count, as well as set the new index field when creating a new one

--- src/services/tasks-service.ts
+++ src/services/tasks-service.ts
@@ -1,6 +1,7 @@
 import { BacklogTaskItem } from '@/types/tasks';
 import { db } from '@/lib/firebase';
-import { collection, doc, getDocs, setDoc, serverTimestamp } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, setDoc, updateDoc, serverTimestamp } from 'firebase/firestore';
+import _ from 'lodash';
 
 export async function getUserBacklogItems(accountId: string): Promise<Array<BacklogTaskItem>> {
   // Fetch the users entire backlog
@@ -13,6 +14,9 @@ export async function getUserBacklogItems(accountId: string): Promise<Array<Back
     ...(doc.data() as BacklogTaskItem),
   }));
 
+  // Keep the list organized for display. Firestore auto sorts alphabetically by ID
+  backlogItems = _.orderBy(backlogItems, ['index'], ['desc']);
+
   return backlogItems;
 }
 
@@ -20,11 +24,33 @@ export async function getUserBacklogItems(accountId: string): Promise<Array<Back
 export async function createNewBacklogItem(accountId: string, newItemDescription: string) {
   // Doing this one separately so I can grab the generated id
   let newDocRef = doc(collection(db, 'backlog', accountId, 'tasks'));
+  let taskCount = await getBacklogTaskCount(accountId);
 
   await setDoc(newDocRef, {
     dateAdded: serverTimestamp(),
     description: newItemDescription,
     id: newDocRef.id,
+    index: taskCount,
     tag: '',
   });
+
+  // Need to increase the task count now that we have added a new one
+  updateBacklogTaskCount(accountId, taskCount + 1);
+}
+
+async function getBacklogTaskCount(accountId: string): Promise<number> {
+    let backlogTaskCount = -1;
+    let backlogSnapshot = await getDoc(doc(db, 'backlog', accountId));
+    if(backlogSnapshot.data()) {
+        let backlogData = backlogSnapshot.data();
+        backlogTaskCount = backlogData?.taskCount;
+    }
+
+    return backlogTaskCount;
+}
+
+function updateBacklogTaskCount(accountId: string, newTaskCount: number) {
+    updateDoc(doc(db, 'backlog', accountId), {
+        taskCount: newTaskCount
+    });
 }
